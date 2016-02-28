27e191bbd99db90942e8ce5d7d806c49596337b9
zcopro <zcoouhatafe@gmail.com>
2016-02-28 23:09:02 +0800

    bat for Submodul update

--- update-submodule.bat
+++ update-submodule.bat
@@ -1,1 +1,7 @@
 git submodule foreach --recursive git submodule update --init
+
+git -c diff.mnemonicprefix=false -c core.quotepath=false fetch origin
+
+git -c diff.mnemonicprefix=false -c core.quotepath=false pull origin master
+
+git -c diff.mnemonicprefix=false -c core.quotepath=false submodule update --init --recursive
